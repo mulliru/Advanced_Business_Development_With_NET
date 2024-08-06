4f0be624b3e59db7a5bad1817518ef346fba826d
Murillo Ferreira <murilloferreira348@gmail.com>
2024-08-06 12:01:33 -0300

    Aula do dia 06/08/24 Fianlizada

---
+++ Primeiro_Semestre/Aula_01/InciandoComCsharp/InciandoComCsharp/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!"); // No Java, seria System.out.println("Hello, World!");
+
+string? texto = Console.ReadLine();
+Console.WriteLine(texto);
+
+// String simples
+var nome = "Leonardo";
+
+// String multi-linha
+var textoMultilinha = """
+                      Olá mundo,
+                      meu nome é Murillo,
+                      e estou aqui para aprender C#!
+                      """;
+
+Console.WriteLine(textoMultilinha);
+
+// String interpolada
+var textoInterpolado = $"Olá, meu nome é {nome}";
+Console.WriteLine(textoInterpolado);
+
+// Tipos primitivos
+int idade = 19;
+double altura = 1.76;
+bool maiorDeIdade = true;
+char sexo = 'M';
+string nome_ = "Murillo";
